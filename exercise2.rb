a13dff3e2c61050e33c9ee543314c457d9cfb1b9
Moe <kenol.maurice@gmail.com>
2014-06-03 14:21:24 -0400

    Tip

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#How would you calculate a good tip for a 55 dollar meal? puts "Tip is 13% of
+puts "Tip is 13% of the total bill,  and your bill was 55$" 
+tip = (55 * 1.13) - 55
+
+puts "You should tip #{tip}"
+
+# Try adding a string and an integer. What happens? Find a way to convert the
+# integer so that it works and use puts to print the answer onscreen.
+
+
+
