7ef145984f1f386572f8c9e1ee791316fb25c2a0
Moe <kenol.maurice@gmail.com>
2014-06-05 14:18:07 -0400

    A better first copy

--- people.rb
+++ people.rb
@@ -1,5 +1,23 @@
 class Student
+	def learn
+		"I get it!"
+	end
+
+	def to_s
+		learn.to_s
+	end
 end
 
-class Instructor
+class Teacher
+	def teach
+		"Everything in Ruby is an Object"
+	end
+
+	def to_s
+		teach.to_s
+	end
 end
+
+puts Student.new
+
+puts Teacher.new
